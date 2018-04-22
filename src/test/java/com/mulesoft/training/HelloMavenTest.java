3de317f52366b8972d3edae9376f02b2a0812a8f
unknown <ruchi0@yahoo.com>
2018-04-22 17:31:54 +0530

    Added header validation test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,8 +1,9 @@
 package com.mulesoft.training;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -11,6 +12,15 @@ public class HelloMavenTest extends FunctionalTestCase {
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello World");
     }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("mavenFlow");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      //assertEquals("text/plain", contentType);
+      assertEquals("text/xml", contentType);
+    }
+
     
     @Override
     protected String getConfigFile() {
